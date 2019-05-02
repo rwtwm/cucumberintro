9c971601a2bbf54ea97b3553b9b7ed4d4c6c8c24
Waine James <wjames@spartaglobal.com>
2019-05-02 09:42:14 +0100

    Create first feature file and stub step definitions.

---
+++ src/test/java/CucumberStepDefs.java
@@ -0,0 +1,24 @@
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+
+public class CucumberStepDefs
+{
+    @Given("cucumber is set up correctly")
+    public void cucumber_is_set_up_correctly() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("I run the tests")
+    public void i_run_the_tests() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("I can see them run")
+    public void i_can_see_them_run() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+}
---
+++ src/test/resources/features/cucumberintro.feature
@@ -0,0 +1,8 @@
+Feature: Cucumber investigation.
+  As a user of cucumber
+  I want to learn how to use its features.
+  Scenario: I am able to run a test in cucumber.
+    Given cucumber is set up correctly
+    When I run the tests
+    Then I can see them run
+
